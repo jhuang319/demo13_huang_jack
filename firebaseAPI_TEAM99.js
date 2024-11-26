600cc7331be5092d55e375ba594e5714e58b1b3b
jhuang319 <exmortem22@gmail.com>
2024-11-26 13:26:21 -0800

    Create firebaseAPI_TEAM99.js

---
+++ firebaseAPI_TEAM99.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1jx6V8DgKQM7X7rfSf6UXt37kN31FYsI",
+  authDomain: "comp1800-133d2.firebaseapp.com",
+  projectId: "comp1800-133d2",
+  storageBucket: "comp1800-133d2.firebasestorage.app",
+  messagingSenderId: "180299843839",
+  appId: "1:180299843839:web:b8a61279e32f18558464ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
